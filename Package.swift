a4445e2372cd0f5b29f330f808ade88563ab647f
Carson B Dunlap <cbd135.dev@gmail.com>
2024-09-23 10:14:09 -0400

    initial commit

---
+++ Package.swift
@@ -0,0 +1,10 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "FontBlaster",
+    platforms: [.iOS(.v11), .tvOS(.v11), .macOS(.v10_10)],
+    products: [.library(name: "FontBlaster", targets: ["FontBlaster"])],
+    targets: [.target(name: "FontBlaster", path: "Sources")],
+    swiftLanguageVersions: [.v5]
+)
